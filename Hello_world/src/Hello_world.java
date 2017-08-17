27235062fa7c39277431832b1fd5686cc4f73ff0
stevenwyee <stevenyeesteven@gmail.com>
2017-08-16 18:57:54 -0700

    8/16/17 Hello world demonstration
    Quick introduction of adding and running code on Eclipse.

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,14 @@
+/**
+	First coding of APCS.
+	@author Steven
+	@version 1.0 8/16/17
+*/
+
+public class Hello_world {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Steven");
+//Display the string
+    }
+}
+
