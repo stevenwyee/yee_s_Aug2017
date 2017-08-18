66f777fe90bfdbe603df246b06aebe9fa6a8cff0
stevenwyee <31084278+stevenwyee@users.noreply.github.com>
2017-08-18 09:52:13 -0700

    First Project
    Basic Hello World program that prints.

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -1,14 +1,15 @@
 /**
-	First coding of APCS.
-	@author Steven
-	@version 1.0 8/16/17
-*/
+ * Prints out a hello from the coder on screen
+ * 
+ * @author Steven Yee 
+ * @version 1.0 8/18/17
+ */
 
-public class Hello_world {
+public class Hello_World
+{
     public static void main(String[] args)
     {
-        System.out.println("Hello World! From Steven");
-//Display the string
+        System.out.println("Hello World! From Steven"); 
+        //Display the string
     }
 }
-
