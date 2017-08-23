8805459b415850e8d19c460d61a428286fc1f88e
stevenwyee <31084278+stevenwyee@users.noreply.github.com>
2017-08-23 10:51:19 -0700

    Project 2

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,12 @@
+//Steven Yee
+//8.23.17
+//Project 2 -- Use print and println
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.print("Hi there");
+		System.out.println();
+		System.out.println("Hi there!");
+	}
+
+}
