9581e8e1c06c8484afad0c71ba544e292835c92a
stevenwyee <31084278+stevenwyee@users.noreply.github.com>
2017-08-24 09:30:37 -0700

    Third time's a charm :)

---
+++ Project2/Project2.java
@@ -0,0 +1,14 @@
+//Steven Yee
+//August 24, 2017
+/*	This class compares output using print and println
+ */
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("It's Steven.");
+
+	}
+
+}
